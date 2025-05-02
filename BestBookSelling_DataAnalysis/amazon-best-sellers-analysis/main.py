10c14a322e5040a04face26347f510be701fd700
Fariha <alamfariha560@gmail.com>
2025-05-02 01:26:02 +0100

    Update main.py

--- BestBookSelling_DataAnalysis/amazon-best-sellers-analysis/main.py
+++ BestBookSelling_DataAnalysis/amazon-best-sellers-analysis/main.py
@@ -14,21 +14,16 @@ print(df.columns)
 # Get summary statistics for each column
 print(df.describe())
 
-# Step 4
 df.drop_duplicates(inplace=True)
 
 df["Price"] = df["Price"].astype(float)
 
 df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
 
-
-
-# Step 5
 author_counts = df['Author'].value_counts()
 print(author_counts)
 
 avg_rating_by_genre = df.groupby("Genre")["Rating"].mean()
 print(avg_rating_by_genre)
 
-# Step 6
 author_counts.head(10).to_csv("top_authors.csv")
